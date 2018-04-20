7b23fb9d31cccbf5c06c81f149d0426bdb47a63b
Norman Nolasco <nnolasco@advarion.com>
2018-04-20 01:12:24 -0500

    updated rds endpoint

--- flask-aws/bin/activate.fish
+++ flask-aws/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/mnt/c/Python36/flask_tutorial/flask-aws-tutorial/flask-aws"
+set -gx VIRTUAL_ENV "/mnt/c/Python36/flask-apis/flask-aws-tutorial/flask-aws"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
